9e13c0ee16003f63cadfd043db5a1e6383a19a66
Petr Dankin <petr2768@yandex.ru>
2021-11-24 13:09:46 +0300

    initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 20) {
+            if (i > 6 && i < 10) {
+                continue;
+            }
+            System.out.println("YES");
+            i = i + 1;
+        }
+    }
+}
